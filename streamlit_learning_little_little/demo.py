6312656fd090ecfc590937312c36d570960544cd
Vansh Garg <149486213+vaanshgarg@users.noreply.github.com>
2026-07-27 15:44:23 +0530

    Delete streamlit_learning_little_little directory

--- streamlit_learning_little_little/demo.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Expense Management System")
-
-expense_dt = st.date_input("Expense date: ")
-if expense_dt:
-    st.write(f"Fetching expenses for {expense_dt}")
-
